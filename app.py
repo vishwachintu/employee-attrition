dd9fa72bac8809e43c5831fb70443632bc31d9ad
vish <32350453+vishwachintu@users.noreply.github.com>
2022-11-11 00:07:43 +0530

    change added

--- app.py
+++ app.py
@@ -213,6 +213,7 @@ if selected_choice is not None:
         st.markdown("please make sure you have collected **_right_ _data_ _points_** to run the batch predictions")
         st.write("Satisfaction level, Last Evaluation, Number of project, The average montly hours, Time spend in company, Work accident, Promotion last 5 years, Department, Salary Level")
         st.write("Make sure data is in the correct shape")
+        image = Image.open(r'sample.png')
         st.write("Please upload your dataset in the form csv")
         uploaded_file = st.file_uploader("Choose a file")
         if uploaded_file is not None:
